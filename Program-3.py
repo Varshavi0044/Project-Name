23a8a4a05dc191886f449bccbd8926bca8172b3d
Varshavi0044 <varshavarsha20041127@gmail.com>
2025-07-17 14:12:30 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -1,5 +1,5 @@
 def generate_series(a):
     if a % 2 == 0:
-        a -= 1  
-    series = [2 * i + 1 for i in range(a)]
-    return series
+        a -= 1
+        series = [2 * i + 1 for i in range(a)]
+        return series
